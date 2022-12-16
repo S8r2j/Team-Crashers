2c7c75a7ace05a4882bc289bd665414cdef43519
nehashah4590 <ns457989@gmail.com>
2022-12-16 12:27:08 +0545

    added neha password

--- databaseconnection.py
+++ databaseconnection.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-url="postgresql://postgres:s8r2j123@localhost/team_crashers"
+url="postgresql://postgres:1234@localhost/team_crashers"
 
 engine = create_engine(url)
 
